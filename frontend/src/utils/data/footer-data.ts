1f6442724c85a7d29898c653acf5a534b2e397f4
Jaoearn <jaoearn.sp@gmail.com>
2026-03-30 09:38:30 +0700

    Update footer-data.ts

--- frontend/src/utils/data/footer-data.ts
+++ frontend/src/utils/data/footer-data.ts
@@ -1,6 +1,6 @@
 export const footerData = {
-    email: "jaoearn.sp@email.com",
-    phone: "0903996712",
+    email: "jaoearn.sp@gmail.com",
+    phone: "090-399-6712",
     location: "Bangkok, Thailand",
     github: "https://github.com/Jaoearn",
     linkedIn: "https://linkedin.com/in/sutheera-preenan-06bb0126a"
